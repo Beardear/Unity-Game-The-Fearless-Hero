72dfe3a1e0b2f437deca08e626a2cefc6e2c67c9
moran <beardear1994@gmail.com>
2022-09-30 23:58:10 -0500

    add hp slider, and harm to enemies

--- Assets/Code/Enemy.cs
+++ Assets/Code/Enemy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
@@ -9,23 +10,50 @@ public class Enemy : MonoBehaviour
 
     //Outlet
     private NavMeshAgent agent;
+    public Slider slider;
 
+    private int hp = 100;
+    private int hpHolder;
     //Configuration
     //public Transform target;
 
     //Methods
     void Start()
-    {
+    {   
+
         target = GameObject.FindGameObjectWithTag("Player").transform;
 
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
         agent.updateUpAxis = false;
 
+        hpHolder = hp;
+
     }
     void Update()
     {
         agent.SetDestination(target.position);
 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hp < 0) return;
+        hp -= 20;
+        slider.value = (float)hp / hpHolder;//通过改变value的值（float类型）来改变血条长度。
+        if (hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    void TakeDamge(int damage)//当受到伤害
+    {
+        if (hp < 0) return;
+        hp -= damage;
+        slider.value = (float)hp / hpHolder;//通过改变value的值（float类型）来改变血条长度。
+        if (hp <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Assets/Code/PlayerController.cs
+++ Assets/Code/PlayerController.cs
@@ -14,14 +14,15 @@ namespace Platformer
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetKey(KeyCode.UpArrow))
-                transform.position += new Vector3(0, 0.02f, 0);
-            if (Input.GetKey(KeyCode.DownArrow))
-                transform.position += new Vector3(0, -0.02f, 0);
-            if (Input.GetKey(KeyCode.LeftArrow))
-                transform.position += new Vector3(-0.02f, 0, 0);
-            if (Input.GetKey(KeyCode.RightArrow))
-                transform.position += new Vector3(0.02f, 0, 0);
+            if (Input.GetKey(KeyCode.W))
+                transform.position += new Vector3(0, 0.01f, 0);
+            if (Input.GetKey(KeyCode.S))
+                transform.position += new Vector3(0, -0.01f, 0);
+            if (Input.GetKey(KeyCode.A))
+                transform.position += new Vector3(-0.01f, 0, 0);
+            if (Input.GetKey(KeyCode.D))
+                transform.position += new Vector3(0.01f, 0, 0);
         }
     }
+
 }
